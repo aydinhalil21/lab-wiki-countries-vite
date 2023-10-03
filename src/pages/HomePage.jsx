3e19691aca02745e79a6efb0ba36dcb7609f30ff
aydinhalil21 <aydinhalil21@gmail.com>
2023-10-03 09:31:22 +0200

    done!

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
@@ -8,32 +8,28 @@ function HomePage() {
   useEffect(() => {
     axios
       .get("https://ih-countries-api.herokuapp.com/countries")
-      .then((data) => {
-        console.log(data);
-        setCountryData(data?.data);
+      .then((response) => {
+        console.log(response);
+        setCountryData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching country data:", error);
       });
   }, []);
 
   return (
     <>
-      <h1>WikiCountries: Your Guide to the World</h1>
-      <div
-        className="container"
-        style={{ maxHeight: "90vh", overflow: "scroll" }}
-      >
-        <h1 style={{ fontSize: "24px" }}>
-          WikiCountries: Your Guide to the World
-        </h1>
-
+      <div className="container" style={{ maxHeight: "90vh", overflow: "scroll" }}>
+        <h1 style={{ fontSize: "24px" }}>WikiCountries: Your Guide to the World</h1>
         <ul className="list-group">
-          {countryData.map(country => (
-                 <li key={country._id}>
-                  <Link className="navbar-brand" to={`/country-details/${country.alpha3Code}`} ><p className="list-group-item list-group-item-action">{country.name.common}</p></Link >
+          {countryData.map((country) => (
+            <li key={country._id}>
+              <Link className="navbar-brand" to={`/country-details/${country.alpha3Code}`}>
+                <p className="list-group-item list-group-item-action">{country.name.common}</p>
+              </Link>
             </li>
-            ))}
-            </ul>
-
-     
+          ))}
+        </ul>
       </div>
     </>
   );
